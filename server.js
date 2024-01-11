5f1cde55504a290821537c85482e2fbc8d4e3916
Zarman-oss <zarmanimran@gmail.com>
2024-01-12 01:24:12 +0500

    .

--- server.js
+++ server.js
@@ -1,10 +1,20 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import bootcamps from './routes/bootcamps';  
 
+
+//  env variables 
 dotenv.config({ path: './config/config.env' });
 
-const app = express();
+// initialize express app
+const app = express();  
+
+
+// Mount routers 
+
+app.use ('/api/v1/bootcamps', bootcamps);
+
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`));
+app.listen(PORT, () => console.log(`Server is running in ${process.env.NODE_ENV} mode on port ${PORT}`)); 
