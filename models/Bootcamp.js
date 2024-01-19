824ee9310fceff5c5809a325e4f8a08c7bea24fc
Zarman-oss <zarmanimran@gmail.com>
2024-01-20 01:36:49 +0500

    .

--- models/Bootcamp.js
+++ models/Bootcamp.js
@@ -1,13 +1,48 @@
-import mongoose from "mongoose"; 
+import mongoose from 'mongoose';
 
-const BootcampSchema= new mongoose.Schema({
-    name:{
-          type: String, 
-          required: [true, 'Please add a name'], 
-          unique: true,
-          trim: true, 
-          max: [50, "Name can't be more than 50 characters"]       
-    }, 
-    slug: String, 
-    description : ""
-})
+const BootcampSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please add a name'],
+    unique: true,
+    trim: true,
+    maxlength: [50, "Name can't be more than 50 characters"],
+  },
+  slug: String,
+  description: {
+    type: String,
+    required: [true, 'Please add a description'],
+    maxlength: [50, 'Name cant be more than 50 characters'],
+  },
+  website: {
+    type: String,
+    match: [
+      /^https:\/\/\S+$/,
+      'Please enter a valid website URL starting with "https://"',
+    ],
+  },
+  email: {
+    type: String,
+    match: [/^\S+@\S+\.\S+$/, 'Please enter a valid email address'],
+  },
+  phone: {
+    type: String,
+    maxlength: [20, 'Phone number can not be longer than 20 characters'],
+  },
+
+  address: {
+    type: String,
+    required: [true, 'Please add an address'],
+  },
+  location: {
+    type: {
+      type: String,
+      enum: ['Point'],
+      required: true,
+    },
+    coordinates: {
+      type: [Number],
+      required: true,
+    },
+  },
+});
