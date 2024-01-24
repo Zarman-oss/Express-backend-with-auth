757b4b67f899337884bf8b66215f511eb1da0e2b
Zarman-oss <zarmanimran@gmail.com>
2024-01-24 10:57:39 +0500

    .

--- utils/geocoder.js
+++ utils/geocoder.js
@@ -1,1 +1,11 @@
-import NodeGeocoder from 'ip-geolocation-api-javascript-sdk/GeolocationParams.js';
+import axios from 'axios';
+axios
+  .get(
+    'https://ipgeolocation.abstractapi.com/v1/?api_key=ca359a09d9574794991771551f38a1e0&ip_address=39.46.181.190'
+  )
+  .then((response) => {
+    console.log(response.data);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
