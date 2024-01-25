4a8da0b7125ebacf504edd002fc3b0ccb0b5592f
Zarman-oss <zarmanimran@gmail.com>
2024-01-25 13:03:57 +0500

    .

--- seeder.js
+++ seeder.js
@@ -1,40 +1,85 @@
-import fs from 'fs';
+// import mongoose from 'mongoose';
+// import colors from 'colors';
+// import path from 'path';
+// import dotenv from 'dotenv';
+// import Bootcamp from './models/bootcampModel.js';
+// import bootcampData from './data/bootcamps.js';
+
+// // ?Load environment variables from .env file
+// dotenv.config();
+
+// // ?Connect to the database
+// mongoose.connect(process.env.MONGO_URI);
+
+// // ?Function to import data to the database
+// const importData = async () => {
+//   try {
+//     await Bootcamp.create(bootcampData); // Fixed typo (bootcamps -> bootcampData)
+//     console.log('Data Imported...'.green.inverse);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// // ?Function to delete data in the database
+// const deleteData = async () => {
+//   try {
+//     await Bootcamp.deleteMany();
+//     console.log('Data Destroyed...'.red.inverse);
+//   } catch (err) {
+//     console.error(err);
+//   }
+// };
+
+// // ?Check command line arguments and perform actions accordingly
+// if (process.argv[2] === '-i') {
+//   importData();
+// } else if (process.argv[2] === '-d') {
+//   deleteData();
+// }
 import mongoose from 'mongoose';
 import colors from 'colors';
+import path from 'path';
 import dotenv from 'dotenv';
 import Bootcamp from './models/bootcampModel.js';
+import bootcampData from './data/bootcamps.js';
 
-// ?Load environment variables from .env file since I have my .env file in root
-
+// Load environment variables from .env file
 dotenv.config();
 
-// ?Load models
-
-// *Connect to data base
-mongoose.connect(process.env.MONGO_URI);
-
-//?read and parse the json files
-
-const bootcamps = JSON.parse(
-  fs.readFileSync(`${__dirname} /_data/ bootcamps.json`, 'utf-8')
-);
-
-// ? To import data to db
-const importData = async () => {
+const connectDB = async () => {
   try {
-    await Bootcamp.create(bootcamps);
-    console.log('Data Imported...'.green.inverse);
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('Connected to the database...'.cyan.inverse);
   } catch (err) {
-    console.log(err);
+    console.error(
+      `Error connecting to the database: ${err.message}`.red.inverse
+    );
+    process.exit(1);
   }
 };
-// ? To delete data in db
+
+const importData = async () => {
+  try {
+    await connectDB();
+    await Bootcamp.create(bootcampData);
+    console.log('Data Imported...'.green.inverse);
+  } catch (err) {
+    console.error(`Error importing data: ${err.message}`.red.inverse);
+  } finally {
+    mongoose.connection.close();
+  }
+};
+
 const deleteData = async () => {
   try {
-    await bootcamp.deleteMany();
+    await connectDB();
+    await Bootcamp.deleteMany();
     console.log('Data Destroyed...'.red.inverse);
   } catch (err) {
-    console.log(err);
+    console.error(`Error deleting data: ${err.message}`.red.inverse);
+  } finally {
+    mongoose.connection.close();
   }
 };
 
